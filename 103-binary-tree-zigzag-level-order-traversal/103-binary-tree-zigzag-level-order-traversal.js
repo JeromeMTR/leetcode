cd49e00e441addbebe1482a70783ce0f6a6b26e7
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-04 12:25:25 -0700

    Time: 122 ms (10.91%), Space: 44 MB (58.17%) - LeetHub

---
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.js
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function(root) {
+  // i = root node
+  // o = zigzag level order traversal of node vals  
+  // c = most optimal solution possible
+  // e = return empty array if root is null
+  
+  if (!root) return [];
+  
+  // keep track of results
+  // keep track of current node
+  // declare an array to make it function like a queue
+  let node = root,
+      results = [],
+      queue = [root];
+  
+  // keep traversing while there is something inside the queue
+  while (queue.length) {
+    // keep track of current levels value
+    // check length of results
+    const levelVals = [],
+          length = queue.length,
+          isOdd = results.length % 2 === 1; 
+      
+    for (let i = 0; i < length; i++) {
+      node = queue.shift();
+      // push or unshift val into levelVals depending on odd
+      isOdd ? levelVals.unshift(node.val) : levelVals.push(node.val);
+      
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    // push levelVals array into results
+    results.push(levelVals)
+  }
+  // return results
+  return results;
+};
