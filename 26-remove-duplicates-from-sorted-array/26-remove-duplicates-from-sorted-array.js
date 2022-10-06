0df2755f81ca9bc79c200ff0dc7ef4f65393892c
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-06 07:41:46 -0700

    Time: 102 ms (79.64%), Space: 44.5 MB (68.57%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  // i = array
+  // o = array of unique nums in array
+  // c = sorted array, try to solve first and then optimize
+  // e = if array length is less than 1 return [] array
+  
+  
+  let i = 0,
+      nonDup = 1,
+      results = [];
+  
+  while (i < nums.length) {
+    // check if prev index of non dup matches current index
+    if (nums[nonDup - 1] !== nums[i]) {
+      // swap the current index element 
+      nums[nonDup] = nums[i];
+      // increment nonDup
+      nonDup++;
+      results.push(nums[nonDup - 1]);
+    }
+    
+    // increase i count to eventually stop loop
+    i++;
+  }
+  
+  return nonDup;
+};
