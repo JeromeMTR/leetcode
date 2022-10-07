cc27d50c0eb766a779ac0006dd79e90187cd2f7f
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-06 19:20:29 -0700

    Time: 133 ms (22.49%), Space: 44.8 MB (75.52%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  // i = head node of linked list
+  // o = true or false depending on cycle or not
+  // c = try to solve brute force and then find optimiztions
+  // e = if head nodes next is null return false
+  
+  if (!head || !head.next) return false;
+    
+  let fast = head,
+      slow = head;
+  
+  // loop through the link if fast node is not null and fast next is also not null
+  while (fast !== null && fast.next !== null) {
+    // reassign fast pointer first
+    fast = fast.next.next;   
+    // reassign slow pointer second
+    slow = slow.next;
+    
+    // if both pointers node match
+      // return true
+    if (fast === slow) return true
+  }
+  // return false if loop breaks  
+  return false;
+};
