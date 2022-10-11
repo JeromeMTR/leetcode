f92d86f7f9a4aa65d6a271474b5ab3df33f5b091
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-11 10:06:56 -0700

    Time: 226 ms (60.01%), Space: 85.7 MB (28.12%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  // i = head of linked list
+  // o = true or false depending palindrome
+  // c = find optimized solution
+  // e = if head is null or head.next is null return false
+  
+  // keep track of current node
+  let node = head,
+      str = '';
+  // push everything into string
+  while (node) {
+    str += node.val;
+    node = node.next;
+  }
+  // reference two pointers front and back
+  let front = 0,
+      back = str.length - 1
+  
+  // loop until front and back is equal to each other
+  while (front <= back) {
+    // if front and back elements don't match
+    if (str[front] !== str[back]) return false;
+      // return false
+    // increment front and decrement back
+    front++;
+    back--;
+  }
+  // return true if loop breaks
+  return true;
+  
+  // declare a fast and slow reference
+  // let slow = head,
+  //     fast = head;
+  // // find middle of linked list
+  // while (fast && fast.next) {
+  //   slow = slow.next;
+  //   fast = fast.next.next;
+  // }
+  // reverse second half
+  
+  // compare first half and second half to see if it is a palindrom
+  
+  // reverse second half again to revert linked list
+  
+};
