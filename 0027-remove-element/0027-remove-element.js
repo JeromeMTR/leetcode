d9316f79b1b6cadce44cadc9acde28e08e1df1b2
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-26 09:18:42 -0700

    Time: 98 ms (49.75%), Space: 43.2 MB (6.35%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -33,5 +33,8 @@ var removeElement = function(nums, val) {
     }
   }
   
-  return swapCount > 0 ? nums.slice(0, -swapCount).length : nums.length
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) return nums.slice(0, i).length;
+  }
+  // return swapCount > 0 ? nums.slice(0, -swapCount).length : nums.length
 };
