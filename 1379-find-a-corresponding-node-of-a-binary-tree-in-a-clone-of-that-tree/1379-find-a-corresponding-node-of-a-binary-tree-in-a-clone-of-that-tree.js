4e9faa7d8454eed4215db24b87953ecfd8fcdc33
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-27 08:05:23 -0700

    Time: 569 ms (6.29%), Space: 88 MB (9.71%) - LeetHub

---
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function(original, cloned, target) {
+  // i: og and clone bst, target node in original tree
+  // o: return same node val in cloned tree
+  // c: find solution and then optmize
+  // e: if original tree is empty return empty array
+  if (!original.left & !original.right) return cloned;
+  
+  const queue = [cloned];
+  let ref;
+  
+  while (queue.length > 0) {
+    let curNode = queue.shift();
+    
+    if (curNode.val === target.val) return curNode;
+    
+    if (curNode.right) queue.push(curNode.right);
+    if (curNode.left) queue.push(curNode.left);
+    
+  }
+};
