47bdc10e6b8e135f34a7befd499cc94c11c7f948
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-28 19:53:18 -0700

    Time: 122 ms (5.74%), Space: 42.7 MB (6.66%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  const result = [];
+  
+  let digitsJoined = digits.join('');
+  console.log(digitsJoined);
+  digitsJoined = (BigInt(digitsJoined) + BigInt(1)).toString();
+  for (let i = 0; i < digitsJoined.length; i++)  {
+    result.push(digitsJoined[i]);
+  }
+  return result;
+};
