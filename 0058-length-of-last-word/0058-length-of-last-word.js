8efa04c30c12e6b84aeba5684f7696d254065925
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-29 19:39:36 -0700

    Time: 116 ms (8.74%), Space: 44.8 MB (5.04%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  // i: string input with spaces
+  // o: lenght of last word in string
+  // c: find solution and then optimize
+  // e: assume there will at least be one word in s
+  let len;
+  
+  let splitS = s.split(' ');
+  console.log(splitS);
+  let filter = splitS.filter(word => word);
+  console.log(filter);
+  return filter[filter.length - 1].length;
+};
