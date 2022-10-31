8b5cb93837c8e3bdf2d606f8e6f929edc2033ed9
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-10-30 19:50:48 -0700

    Time: 85 ms (71.95%), Space: 42.3 MB (46.76%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  // i: number array and target number
+  // o: index of target array if its in nums array, if not where it would be
+  // c: find solution and then optmize
+  // e: assume that there is at least one element
+  let halfCount = Math.floor(nums.length/2);
+  let halfElement = nums[halfCount];
+  
+  if (target > halfElement) {
+    while (nums[halfCount] < target) {
+      if (nums[halfCount] === target) return halfCount;
+      halfCount++;
+    }
+  } else {
+    while (nums[halfCount] > target) {
+      if (nums[halfCount] === target) return halfCount;
+      halfCount--;
+    }
+  }
+  console.log(halfCount);
+  if (target < nums[halfCount] && halfCount === 0) return 0;
+  if (halfCount === - 1) return 0;
+  if (target > nums[halfCount]) return halfCount + 1;
+  if (target < nums[halfCount]) return halfCount;
+  return halfCount
+};
