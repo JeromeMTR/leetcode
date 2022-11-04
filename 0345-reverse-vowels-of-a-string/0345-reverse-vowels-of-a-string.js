1bb1a0beeb9759c3fffe5158654c963faca2f82f
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-11-03 19:55:22 -0700

    Time: 149 ms (42.15%), Space: 50.3 MB (34.61%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let sArr = s.split('');
+    let left = 0,
+        right = sArr.length - 1;
+    while (left < right) {
+        if (vowels.includes(sArr[left].toLowerCase()) && vowels.includes(sArr[right].toLowerCase())) {
+            let temp = sArr[left];
+            sArr[left] = sArr[right];
+            sArr[right] = temp;
+            left++;
+            right--;
+            continue;
+        }
+        if (vowels.includes(sArr[left].toLowerCase())) {
+            right--;
+            continue;
+        }
+        if (vowels.includes(sArr[right].toLowerCase())) {
+            left++;
+            continue;
+        }
+        left++;
+        right--;
+    }
+
+    return sArr.join('');
+};
