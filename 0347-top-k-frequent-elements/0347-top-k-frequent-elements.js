c4c7f41145eee00531e843486355a19b87630638
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-11-16 20:19:50 -0800

    Time: 199 ms (7.76%), Space: 48.7 MB (18.00%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -9,19 +9,34 @@ var topKFrequent = function(nums, k) {
   // c: try to solve the complexity better than O(n log n)
   // e: assume that nums will have at least 1 element, and k is in range
   // do we assume if the array is sorted?
+  if (nums.length === 1 && k === 1) return nums;
+  // declare res array
+  let arr = [];
   
-  // declare result array
-  let hash = {}
-    for (let i = 0 ; i < nums.length; i ++) {
-        hash[nums[i]] = hash[nums[i]] + 1 || 1
+  // instantiate a freqMap
+  let freqMap = new Map();
+  
+  for (let i = 0; i < nums.length; i++) {
+    freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
+  }
+  
+  // create bucket with length of nums
+  let bucket = [];
+  for (let i = 0; i <= nums.length; i++) {
+    bucket.push([]);
+  }
+  
+  freqMap.forEach((val, key) => {
+    bucket[val].push(key);
+  })
+  
+  for (let i = bucket.length - 1; i >= 1; i--) {
+    for (let j = 0; j < bucket[i].length; j++) {
+      const cap = arr.length === k;
+      if (cap) break;
+      arr.push(bucket[i][j])
     }
-    let result = [];
-    let keys = Object.keys(hash);
-    keys.sort((a, b) => {
-        return hash[a] - hash[b]
-    })
-    while (result.length < k) {
-        result.push(keys.pop())
-    }
-    return result
+  }
+  console.log(bucket);
+  return arr
 };
