2f1e3b37c3af9cc9234d157c9625fa12f19b105e
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-11-19 14:30:34 -0800

    Time: 82 ms (89.40%), Space: 44.1 MB (86.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,6 +3,12 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+  // i: string input
+  // o: boolean depending on palandrome or not
+  // c: time complexity of O(n) 
+  // e: if string is empty return true
+  
+  
   let removeSym = s.replace(/[^a-zA-Z0-9s]/g, '').toLowerCase();
   
   let left = 0,
