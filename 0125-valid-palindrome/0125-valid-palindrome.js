3b1a9f411906c519fc0b0172cc2a95709de6b8d6
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2022-11-19 14:29:11 -0800

    Time: 89 ms (84.39%), Space: 44.4 MB (75.07%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -5,14 +5,8 @@
 var isPalindrome = function(s) {
   let removeSym = s.replace(/[^a-zA-Z0-9s]/g, '').toLowerCase();
   
-  // for (let i = 0; i < s.length; i++) {
-  //   const letter = checkLetter(s, s[i], i);
-  //   if (letter) removeSym += letter; 
-  // }
-
   let left = 0,
-      right = removeSym.length - 1; 
-  console.log(removeSym)
+      right = removeSym.length - 1;
   while (left < right) {
     if (removeSym[left] !== removeSym[right]) return false;
     left++;
@@ -20,13 +14,3 @@ var isPalindrome = function(s) {
   }
   return true;
 };
-  
-// function checkLetter(s, char, i) {
-//   let num = char * 1;
-//   if (typeof num === 'number') return char;
-//   let code = s.charCodeAt(i);
-//   if (code >= 65 && code <= 90) return char.toLowerCase();
-//   if (code >= 97 && code <= 122) return char.toLowerCase();
-
-//   return false;
-// }
