fde5504399099afed998795f8877b7eb5ee205ee
Jerome Rodriguez <jeromemtrodriguez@gmail.com>
2023-01-16 12:17:23 -0800

    Time: 48 ms (43.24%), Space: 13.9 MB (8.10%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,26 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+      
+      str_num = str(num)
+      
+      # create recursive function 
+      
+      def checkSingle(string: str) -> str:
+        if len(string) == 1:
+          return int(string)
+      
+        total = 0
+        
+        for letter in string: 
+          print(type(int(letter)))
+          total += int(letter)
+          
+        cur_string = str(total)
+        
+        return checkSingle(cur_string)
+      
+      return checkSingle(str_num)
+          
+          
+        
+        
